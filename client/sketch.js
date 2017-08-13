4a099b6dd77b0285aea3f690ef0c779e5d6628db
Rupert Lloyd <rupert@dds.mil>
2017-08-13 15:23:41 -0400

    test a custom draw function

--- client/sketch.js
+++ client/sketch.js
@@ -15,7 +15,7 @@ function draw() {
   background(200);
   while(packet_queue.length>0) {
     console.info(packet_queue.shift());
-    var s = createSprite(mouseX, mouseY, 30, 30);
+    var s = createSprite(width/2, height/2, 30, 30);
     s.velocity.x = random(-5, 5);
     s.velocity.y = random(-5, 5);
   }
@@ -23,14 +23,5 @@ function draw() {
 }
 
 function mousePressed() {
-  
-  //create a sprite at the mouse position and store it in a temporary variable
-  var s = createSprite(mouseX, mouseY, 30, 30);
-  //if no image or animation is associated it will be a rectancle of the specified size
-  //and a random color
-  
-  //now you can use the variable to set properties
-  //e.g. a random velocity on the x and y coordinates
-  s.velocity.x = random(-5, 5);
-  s.velocity.y = random(-5, 5);
+
 }
