46c8a802ee78478b2410f23952d59bf5f266a0b2
Rupert Lloyd <rupert@dds.mil>
2017-08-13 10:26:50 -0400

    add p5.play library

--- client/sketch.js
+++ client/sketch.js
@@ -15,5 +15,22 @@ function draw() {
   background(200);
   while(packet_queue.length>0) {
     console.info(packet_queue.shift());
+    var s = createSprite(mouseX, mouseY, 30, 30);
+    s.velocity.x = random(-5, 5);
+    s.velocity.y = random(-5, 5);
   }
+  drawSprites();
+}
+
+function mousePressed() {
+  
+  //create a sprite at the mouse position and store it in a temporary variable
+  var s = createSprite(mouseX, mouseY, 30, 30);
+  //if no image or animation is associated it will be a rectancle of the specified size
+  //and a random color
+  
+  //now you can use the variable to set properties
+  //e.g. a random velocity on the x and y coordinates
+  s.velocity.x = random(-5, 5);
+  s.velocity.y = random(-5, 5);
 }
